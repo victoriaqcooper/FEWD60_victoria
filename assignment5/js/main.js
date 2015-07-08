c95388dfda493838a8572ff6054f7e62f03a0287
VQ Cooper <vqcoopercs@gmail.com>
2015-07-08 13:44:50 -0400

    assign 5 done

--- assignment5/js/main.js
+++ assignment5/js/main.js
@@ -9,38 +9,49 @@ $(document).ready(function() {
     console.log(city);
 
 //if value of city matches "New York" or "NYC" or "New York City"
-    if(city == 'New York' || 'NYC' || 'New York City') {
+    if(city == 'New York' || city == 'NYC' || city == 'New York City') {
 
-//change class     
+//change background image to nyc.jpg     
       $('body').css("background-image", "url(../assignment5/images/nyc.jpg)");    
     }
 
 //else if value of city matches "San Francisco" or "SF" or "Bay Area"
-    else if(city == 'San Francisco' || 'SF' || 'Bay Area') {
+    else if(city == 'San Francisco' || city == 'SF' || city == 'Bay Area') {
 
-//change class 
+//change background image to sf.jpg 
       $('body').css("background-image", "url(../assignment5/images/sf.jpg)");
     }
 
-    //else if value of city matches "Los Angeles" or "LA" or "LAX"
-        //change class 
+   
+//else if value of city matches "Los Angeles" or "LA" or "LAX"
+    else if(city == 'Los Angeles' || city == 'LA' || city == 'LAX') {
 
-      //else if value of city matches "Austin" or "ATX"
-        //change class 
+//change background image to la.jpg       
+      $('body').css("background-image", "url(../assignment5/images/la.jpg)");
+    }
 
-      //else value of city matches "Sydney" or "SYD"
-        //change class
+//else if value of city matches "Austin" or "ATX"
+    else if(city == 'Austin' || city == 'ATX') {
+//change class 
+      $('body').css("background-image", "url(../assignment5/images/austin.jpg)");
+    }
 
+//else value of city matches "Sydney" or "SYD"
+    else if(city == 'Sydney' || city == 'SYD'){
+
+//change class
+      $('body').css("background-image", "url(../assignment5/images/sydney.jpg)");
+    }
 
     else {
-      $('body').css("background-color", "black");
+      $('body').css("background", "green");
     }
-  });
 
+
+  });
 
 
 
       
   }); 
 
-//$('body').css("background-image", "url(../assignment5/images/nyc.jpg)");
